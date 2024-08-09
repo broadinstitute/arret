112464d1a4a60a8ff8e353dadde4c487c9b97247
Devin McCabe <dmccabe@broadinstitute.org>
2024-08-09 14:42:58 -0400

    simpler to_delete logic

--- arret/plan.py
+++ arret/plan.py
@@ -116,91 +116,13 @@ def make_plan(
         r"/pipelines-logs/[^/]", regex=True
     )
 
-    # split blob names by `/` to columns
-    max_name_depth = inv["name"].str.count("/").max()
-    name_parts = inv["name"].str.split("/", expand=True, regex=False)
-    name_parts.columns = ["name" + str(x) for x in name_parts.columns]
-    name_part_cols = list(name_parts.columns)
-    inv = pd.concat([inv, name_parts], axis=1)
+    inv["to_delete"] = (
+        inv["deletable_empty"]
+        | inv["deletable_pipeline_logs"]
+        | inv["deletable_large"]
+        | (inv["deletable_age"] & inv["deletable_not_kept"])
+    )
 
-    ops = []  # start collecting delete/keep operations
-
-    # iterate over `name0`...`nameN` columns
-    for cix in range(max_name_depth + 1):
-        # for each blob, keep track of the path we're considering this iteration (path
-        # could be a GCS folder or a file)
-        if cix == 0:
-            inv["path"] = inv[name_part_cols[cix]]
-        else:
-            inv["path"] = inv["path"] + "/" + inv[name_part_cols[cix]]
-
-        # group by current paths
-        inv = inv.set_index(name_part_cols[cix], append=cix > 0)
-        inv_g = inv.groupby(inv.index.names)
-
-        # for each group, check are all blobs in that path deletable (for different
-        # kinds of deletability)
-        all_deletable = inv_g["deletable"].all()
-        all_deletable_age = inv_g["deletable_age"].all()
-        all_deletable_empty = inv_g["deletable_empty"].all()
-        all_deletable_large = inv_g["deletable_large"].all()
-        all_deletable_not_kept = inv_g["deletable_not_kept"].all()
-        all_deletable_pipeline_logs = inv_g["deletable_pipeline_logs"].all()
-
-        # collect stats/deletability for each group
-        total_size = inv_g["size"].sum()
-        last_updated = inv_g["updated"].max()
-        n_objs = inv_g.size()
-        n_objs.name = "n_objs"
-
-        stats = pd.concat(
-            [
-                all_deletable,
-                all_deletable_age,
-                all_deletable_empty,
-                all_deletable_large,
-                all_deletable_not_kept,
-                all_deletable_pipeline_logs,
-                total_size,
-                last_updated,
-                n_objs,
-            ],
-            axis=1,
-        )
-
-        stats["depth"] = cix
-
-        # finally mark whether each path should be deleted
-        stats["to_delete"] = (
-            stats["deletable_empty"]
-            | stats["deletable_large"]
-            | stats["deletable_pipeline_logs"]
-            | (stats["deletable_age"] & stats["deletable_not_kept"])
-        )
-
-        # also indicate whether a path represents a single file and we're keeping it
-        stats["to_keep"] = stats["n_objs"].eq(1) & ~stats["to_delete"]
-
-        if stats["to_delete"].any():
-            # collect all the blob names under each deletable path
-            blobs = (
-                (
-                    inv.loc[stats.loc[stats["to_delete"]].index]
-                    .groupby(inv.index.names)["name"]
-                    .agg(list)
-                )
-                .to_frame()
-                .rename(columns={"name": "blobs"})
-            )
-
-            stats = stats.join(blobs, how="left")
-
-        # can remove all blobs in inventory if we just marked them as deleted or kept
-        inv = inv.loc[stats.loc[~(stats["to_delete"] | stats["to_keep"])].index]
-
-        ops.append(stats.reset_index(drop=True))
-
-    # return data frame of deletion operations
-    ops = pd.concat(ops)
-    delete_ops = ops.loc[ops["to_delete"]].sort_values("size", ascending=False)
-    return delete_ops.drop(columns=["to_delete", "to_keep"])
+    return inv.loc[inv["to_delete"], ["name", "updated", "size", "gs_url"]].sort_values(
+        "size", ascending=False
+    )
